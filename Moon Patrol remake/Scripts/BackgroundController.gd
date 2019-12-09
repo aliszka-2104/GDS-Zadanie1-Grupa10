dbfebf2fa7ee98b939b0a7bd614fbc3ceeed28fb
aliszka <agata.anna.debiec@gmail.com>
2019-12-09 23:02:48 +0100

    Add jump
    Export player script variables to inspector

--- Moon Patrol remake/Scripts/BackgroundController.gd
+++ Moon Patrol remake/Scripts/BackgroundController.gd
@@ -6,5 +6,4 @@ onready var visibilityNotifier = get_node(visibilityNotifierPath)
 
 
 func _on_VisibilityNotifier2D_screen_exited():
-	print("exited")
 	position = Vector2(position.x+ 3*get_viewport().size.x,position.y)
--- Moon Patrol remake/Scripts/PlayerController.gd
+++ Moon Patrol remake/Scripts/PlayerController.gd
@@ -1,19 +1,33 @@
 extends KinematicBody2D
 
-var velocity=Vector2(1,0);
-export var speed = 250;
-export var slowDownSpeed = 100;
-export var speedUpSpeed =300;
+export (int) var base_speed = 500
+export (int) var speed_up_value = 100
+export (int) var slow_down_value = 100
+export (int) var jump_speed = 500
+export (int) var gravity = 1000
+
+var velocity = Vector2()
+var jumping = false
 
 func get_input():
-	if Input.is_action_pressed("speed_up"):
-		speed=speedUpSpeed
-	if Input.is_action_pressed("slow_down"):
-		speed=slowDownSpeed
-	velocity=velocity.normalized()*speed;
+    velocity.x = base_speed
+    var speed_up = Input.is_action_pressed('speed_up')
+    var slow_down = Input.is_action_pressed('slow_down')
+    var jump = Input.is_action_just_pressed('ui_select')
+
+    if jump and is_on_floor():
+        jumping = true
+        velocity.y = -jump_speed
+    if speed_up:
+        velocity.x += speed_up_value
+    if slow_down:
+        velocity.x -= slow_down_value
 
 func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity*delta)
+    get_input()
+    velocity.y += gravity * delta
+    if jumping and is_on_floor():
+        jumping = false
+    velocity = move_and_slide(velocity, Vector2(0, -1))
 	
 	
