2d4ccbe7e94706a9df47e5bfd90678427e0569e6
aliszka <agata.anna.debiec@gmail.com>
2019-12-10 22:25:01 +0100

    Add some graphics and basic shooting system

---
+++ Moon Patrol remake/Scripts/Bullet.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export (int)var  speed = 100
+var velocity = Vector2()
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	velocity.y=-speed
+	translate(velocity*delta)
+	
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- Moon Patrol remake/Scripts/PlayerController.gd
+++ Moon Patrol remake/Scripts/PlayerController.gd
@@ -8,26 +8,32 @@ export (int) var gravity = 1000
 
 var velocity = Vector2()
 var jumping = false
+const BULLET = preload("res://Scenes/Bullet.tscn")
 
 func get_input():
-    velocity.x = base_speed
-    var speed_up = Input.is_action_pressed('speed_up')
-    var slow_down = Input.is_action_pressed('slow_down')
-    var jump = Input.is_action_just_pressed('ui_select')
+	velocity.x = base_speed
+	var speed_up = Input.is_action_pressed('speed_up')
+	var slow_down = Input.is_action_pressed('slow_down')
+	var jump = Input.is_action_just_pressed('ui_jump')
+	var shoot = Input.is_action_just_pressed('ui_shoot')
 
-    if jump and is_on_floor():
-        jumping = true
-        velocity.y = -jump_speed
-    if speed_up:
-        velocity.x += speed_up_value
-    if slow_down:
-        velocity.x -= slow_down_value
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = -jump_speed
+	if speed_up:
+		velocity.x += speed_up_value
+	if slow_down:
+		velocity.x -= slow_down_value
+	if shoot:
+		var bullet = BULLET.instance()
+		bullet.position=$Position2D.global_position()
+		get_parent().add_child(bullet)
 
 func _physics_process(delta):
-    get_input()
-    velocity.y += gravity * delta
-    if jumping and is_on_floor():
-        jumping = false
-    velocity = move_and_slide(velocity, Vector2(0, -1))
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
 	
